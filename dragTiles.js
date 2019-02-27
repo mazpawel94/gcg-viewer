d5bda4a5b57e2cffbb749e3de17d3afeed55311a
mazpawel94 <mazpawel94@gmail.com>
2019-02-27 16:35:26 +0100

    repair problem with existing scroll-y on mobile

--- dragTiles.js
+++ dragTiles.js
@@ -2,7 +2,7 @@
 let active = false, startX, startY, actualDiv, ctualDivStartX, actualDivStartY, actualDivStartOrder;
 // const place = 600/7;
 let place = 75;
-if(window.innerHeight <= 600) {
+if(window.innerHeight <= 720) {
 place = 50;
 }
 const convertOrderToInt = (order) => {
