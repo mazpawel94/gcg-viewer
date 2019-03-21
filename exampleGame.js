1f8093c2ec3d6b5a4f4f426f83a96eda7edebf25
mazpawel94 <mazpawel94@gmail.com>
2019-03-21 14:28:05 +0100

    add demo version with default game

---
+++ exampleGame.js
@@ -0,0 +1,32 @@
+const gameToString = `#player1 Michał Michał
+#player2 D.Pikul D.Pikul
+>Michał: ADELOOY 8H LEADY +20 20
+>D.Pikul: ?CKMŃOS L1 SKOŃCzM. +86 86
+>Michał: ŁOOUZŹŻ 9F ZŻUŁ +32 52
+>D.Pikul: ADIJRZZ 1G JADZI.Z +30 116
+>Michał: IIJOOTŹ K7 I.Ź +15 67
+>D.Pikul: AGHNPRW 5I GRA.H +20 136
+>Michał: IIJOOST 7G SIT +21 88
+>D.Pikul: CEKNNPW -KNNPW +0 136
+>Michał: AIJNOOR 8A RAJONO +29 117
+>D.Pikul: CEEKNOR C3 EREKC.O +28 164
+>Michał: ?ACIIRY 3A nI.RYCIA +60 177
+>D.Pikul: EELNOWZ A8 .OZLEWNE +80 244
+>Michał: AIŁMSUZ A1 MI.US +30 207
+>D.Pikul: AFHMNÓP J3 HA.F +20 264
+>Michał: AIŁŚTWZ E2 T.ŁAŚ +26 233
+>D.Pikul: AELMNÓP 13A .ÓL +16 280
+>Michał: GIIKOWZ C11 GI.Z +7 240
+>D.Pikul: ABDEMNP 15A .MAN +14 294
+>Michał: ĄĘIKOTW 11C .IĘTKĄ +36 276
+>D.Pikul: BDENPPY D1 BE.Y +35 329
+>Michał: ABOPSWY 3L .SP +8 284
+>D.Pikul: DENPPWW 4N EN +6 335
+>Michał: ABĆNOWY D10 W.N +10 294
+>D.Pikul: DPPWW B8 .D +7 342
+>Michał: ABĆOY 6C .O. +8 302
+>D.Pikul: PPWW 6B W... +11 353
+>D.Pikul: PPWW --  -11 342
+>Michał: ABĆY O4 .ABYĆ +39 341
+>Michał:  (PPWW) +12 353
+`
---
+++ scrabbleExample.js
@@ -0,0 +1,439 @@
+
+const previousButton = document.getElementById("previous");
+const previousRack = document.querySelector(".rack.previous ul");
+const file = document.getElementById("file");
+const nick = document.querySelector(".nick");
+const nick2 = document.querySelector(".rack.previous .nick");
+const resultPlayer1 = document.getElementById("result1");
+const resultPlayer2 = document.getElementById("result2");
+const deletionLetter = document.body.querySelectorAll(".deletion-letter");
+const deletion = document.body.querySelector(".deletion");
+const comment = document.querySelector(".comment");
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 593;
+canvas.height = 593;
+ctx.beginPath();
+ctx.fillStyle = "#F8E8C7";
+ctx.font = "28px sans-serif bold";
+
+let player1, player2;
+
+let x = 23;
+let y = 23; //coordinates
+let moveNumber = 0;
+let moves = [];
+const tileWidth = 38;
+
+const points = {
+  A: 1,
+  Ą: 5,
+  B: 3,
+  C: 2,
+  Ć: 6,
+  D: 2,
+  E: 1,
+  Ę: 5,
+  F: 5,
+  G: 3,
+  H: 3,
+  I: 1,
+  J: 3,
+  K: 2,
+  L: 2,
+  Ł: 3,
+  M: 2,
+  N: 1,
+  Ń: 7,
+  O: 1,
+  Ó: 5,
+  P: 2,
+  R: 1,
+  S: 1,
+  Ś: 5,
+  T: 2,
+  U: 3,
+  W: 1,
+  Y: 2,
+  Z: 1,
+  Ź: 9,
+  Ż: 5
+};
+
+const letterToNumber = function(a) {
+  if (!a) return;
+  if (a.toUpperCase() == "A") return 0;
+  if (a.toUpperCase() == "B") return 1;
+  if (a.toUpperCase() == "C") return 2;
+  if (a.toUpperCase() == "D") return 3;
+  if (a.toUpperCase() == "E") return 4;
+  if (a.toUpperCase() == "F") return 5;
+  if (a.toUpperCase() == "G") return 6;
+  if (a.toUpperCase() == "H") return 7;
+  if (a.toUpperCase() == "I") return 8;
+  if (a.toUpperCase() == "J") return 9;
+  if (a.toUpperCase() == "K") return 10;
+  if (a.toUpperCase() == "L") return 11;
+  if (a.toUpperCase() == "M") return 12;
+  if (a.toUpperCase() == "N") return 13;
+  if (a.toUpperCase() == "O") return 14;
+};
+
+const reset = function() {
+  x = 23;
+  y = 23;
+};
+
+const createTileOnRack = function(letter) {
+  const node = document.createElement("li");
+  const sub = document.createElement("sub");
+  const textnode = document.createTextNode(letter);
+  const textsub = document.createTextNode(points[letter] || "");
+  sub.appendChild(textsub);
+  node.appendChild(textnode);
+  node.appendChild(sub);
+  rackActual.appendChild(node);
+};
+
+const setRack = function() {
+  const move = decodeMove(moveNumber);
+  if (move[0].includes("#note")) return;
+  setNick(move[0]);
+  const letters = move[1];
+  [...letters].forEach(letter => {
+    createTileOnRack(letter);
+    deleteLetterInDeletion(letter);
+  });
+  [...document.querySelectorAll(".actual li")].forEach((div, index) => div.style.order = index);
+};
+
+const setNick = function(n) {
+  nick.innerHTML = n.slice(1, -1).replace(/_/g, " ");
+};
+
+const setOldRack = function(nodes) {
+  const oldNodes = document.querySelectorAll(".previous li");
+  [...oldNodes].forEach(node => previousRack.removeChild(node));
+  const word = decodeMove(moveNumber)[3]
+    .replace(/[a-ząężćźżłńóś]/g, "?")
+    .split("");
+  [...nodes].forEach(node => {
+    if (word.indexOf(node.textContent[0]) !== -1) {
+      node.style.backgroundColor = "gray";
+      delete word[word.indexOf(node.textContent[0])];
+    }
+    addLetterInDeletion(node.textContent[0]);
+    node.classList.add("old");
+    previousRack.appendChild(node);
+  });
+  nick2.textContent = nick.innerHTML;
+};
+
+const clearRack = function() {
+  const nodes = document.querySelectorAll(".actual li");
+  [...nodes].forEach(node => rackActual.removeChild(node));
+  setOldRack(nodes);
+  nick2.textContent = nick.innerHTML;
+};
+
+const deleteLetterInDeletion = letter => {
+  if (
+    letter === "+" ||
+    letter === "0" ||
+    letter === "." ||
+    typeof letter === "number"
+  )
+    return;
+  letter = letter.replace(/[a-ząężćźżłńóś?]/, "");
+  const change = [...deletionLetter]
+    .filter(e => !e.classList.contains("deleted"))
+    .find(e => e.textContent === letter);
+  if (change) change.classList.add("deleted");
+};
+
+const addLetterInDeletion = letter => {
+  //cofa skreślenie - przy cofaniu ruchu
+  if (
+    letter === "+" ||
+    letter === "0" ||
+    letter === "." ||
+    typeof letter === "number"
+  )
+    return;
+  letter = letter.replace(/[a-ząężćźżłńóś?]/, "");
+  const change = [...deletionLetter]
+    .filter(e => e.classList.contains("deleted"))
+    .find(e => e.textContent === letter);
+  if (change) change.classList.remove("deleted");
+};
+
+const putCoordinates = function(coordinates) {
+  const lastCoordinate = coordinates.slice(-1);
+  if (isNaN(lastCoordinate))
+    // horizontal
+    return [coordinates.slice(0, -1) - 1, letterToNumber(lastCoordinate), 1];
+  // vertical
+  else return [letterToNumber(coordinates[0]), coordinates.slice(1) - 1, 0];
+};
+
+const decodeMove = index => {
+  //word[0] - nick, [1] - litery, [2] - współrzędne, [3] - główny wyraz, [4] - punkty za ruch, [5] - suma punktów
+  return moves[index].split(" ");
+};
+
+const turnOver = function() {
+  clearMove();
+  moveNumber++;
+  clearRack();
+};
+
+const putStartCoordinates = function(xy) {
+  let horizontal = true;
+  if (xy[2]) {
+    x = x + xy[1] * tileWidth;
+    y = y + xy[0] * tileWidth;
+  } else {
+    x = x + xy[0] * tileWidth;
+    y = y + xy[1] * tileWidth;
+    horizontal = false;
+  }
+  return [x, y, horizontal];
+};
+
+const drawLetterOnBoard = function(newMove, x, y, letter) {
+  if (newMove) ctx.fillStyle = "#E8E847";
+  else ctx.fillStyle = "#F8E8C7";
+  ctx.fillRect(x + 1, y + 1, 36, 36);
+  ctx.fillStyle = "#015B52";
+  ctx.font = "10px sans-serif";
+  let point = points[letter] || "";
+  if (point == "") ctx.fillStyle = "rgba(1, 91, 82, 0.2)";
+  ctx.fillText(point, x + 30, y + 35);
+  ctx.font = "bold 28px sans-serif";
+  if (letter.toUpperCase() == "W")
+    ctx.fillText(letter.toUpperCase(), x + 6, y + 28);
+  else if (letter.toUpperCase() == "I")
+    ctx.fillText(letter.toUpperCase(), x + 16, y + 28);
+  else ctx.fillText(letter.toUpperCase(), x + 10, y + 28);
+};
+
+const drawLettersOnBoard = (moveTab, newMove) => {
+  const word = moveTab[3];
+  const xy = putStartCoordinates(putCoordinates(moveTab[2]));
+  let x = xy[0];
+  let y = xy[1];
+  let horizontal = xy[2];
+  for (let i = 0; i < word.length; i++) {
+    let letter = word[i];
+    if (letter == ".") {
+      //. to litera leżąca na planszy, nie jest rysowana, idziemy dalej pionowo lub poziomo
+      if (horizontal) x += tileWidth;
+      else y += tileWidth;
+      continue;
+    }
+    drawLetterOnBoard(newMove, x, y, letter);
+    if (newMove) deleteLetterInDeletion(letter);
+    if (horizontal) x += tileWidth;
+    else y += tileWidth;
+  }
+  reset();
+};
+
+const showComment = text => {
+  comment.querySelector("span").innerHTML = text;
+  comment.style.display = "flex";
+};
+
+const addPoints = points => {
+  if (points.startsWith("+") || points.startsWith("-")) {
+    if (
+      decodeMove(moveNumber)[0]
+        .slice(1, -1)
+        .replace(/_/g, " ") == player1
+    ) {
+      let p = resultPlayer1.innerHTML;
+      p = parseInt(p) + parseInt(points);
+      resultPlayer1.innerHTML = `${p}(${points})`;
+      resultPlayer2.innerHTML = resultPlayer2.innerHTML.split("(")[0];
+    } else {
+      let p = resultPlayer2.innerHTML;
+      p = parseInt(p) + parseInt(points);
+      resultPlayer2.innerHTML = `${p}(${points})`;
+      resultPlayer1.innerHTML = resultPlayer1.innerHTML.split("(")[0];
+    }
+  }
+};
+
+const removePoints = points => {
+  if (points.startsWith("+") || points.startsWith("-")) {
+    if (
+      decodeMove(moveNumber)[0]
+        .slice(1, -1)
+        .replace(/_/g, " ") == player1
+    ) {
+      let p = resultPlayer1.innerHTML;
+      p = parseInt(p) - parseInt(points);
+      resultPlayer1.innerHTML = p;
+    } else {
+      let p = resultPlayer2.innerHTML;
+      p = parseInt(p) - parseInt(points);
+      resultPlayer2.innerHTML = p;
+    }
+  }
+};
+
+const move = function(index) {
+  let word = decodeMove(index);
+  reset();
+  if (index > 0) drawLettersOnBoard(decodeMove(index - 1), false);
+
+  if (word[2] == "--") {
+    // -- oznacza stratę w poprzednim ruchu
+    turnOver();
+    return;
+  }
+
+  if (word[2].startsWith("-"))
+    //wymiana w poprzednim ruchu
+    showComment(`wymiana ${word[2].slice(1)}`);
+  if (word[0].includes("#note")) {
+    showComment(`${word.join(" ")}`);
+    return;
+  }
+
+  drawLettersOnBoard(word, true);
+  addPoints(word[4]);
+};
+
+const findBlanks = word => {
+  let i = word.indexOf("("),
+    w;
+  if (i > -1) {
+    w = word.slice(0, i) + word[i + 1].toLowerCase() + word.slice(i + 2);
+  }
+  i = String(w).indexOf("(");
+  if (i > -1) {
+    w = w.slice(0, i) + w[i + 1].toLowerCase() + w.slice(i + 2);
+  }
+  return String(w)
+    .replace(")", "")
+    .replace(")", "");
+};
+
+const clearAll = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  rackActual.innerHTML = "";
+  previousRack.innerHTML = "";
+  moveNumber = 0;
+  resultPlayer1.innerHTML = 0;
+  resultPlayer2.innerHTML = 0;
+  deletionLetter.forEach(letter => letter.classList.remove("deleted"));
+};
+
+const showEndResult = () => {
+  const pointsFromWriteOff = parseInt(decodeMove(moveNumber)[3]) / 2;
+  if (
+    decodeMove(moveNumber)[0]
+      .slice(1, -1)
+      .replace(/_/g, " ") == player1
+  )
+    showComment(
+      "Wynik końcowy: <br />" +
+        `${player1} ${parseInt(resultPlayer1.innerHTML) +
+          pointsFromWriteOff} : ${player2} ${parseInt(resultPlayer2.innerHTML) -
+          pointsFromWriteOff}`
+    );
+  else
+    showComment(
+      "Wynik końcowy: <br />" +
+        `${player1} ${parseInt(resultPlayer1.innerHTML) -
+          pointsFromWriteOff} : ${player2} ${parseInt(resultPlayer2.innerHTML) +
+          pointsFromWriteOff}`
+    );
+};
+
+const next = () => {
+  if (moveNumber + 2 > moves.length) return;
+  if (moveNumber + 2 == moves.length) {
+    showEndResult();
+    moveNumber++;
+    return;
+  }
+
+  clearRack();
+  move(moveNumber);
+  moveNumber++;
+  setRack();
+};
+
+const clearMove = () => {
+  if (moveNumber < 1) return;
+  moveNumber--;
+  if (decodeMove(moveNumber)[2] === "--") {
+    moveNumber--;
+    return;
+  }
+  clearRack();
+  const word = decodeMove(moveNumber);
+  if (decodeMove(moveNumber + 1)[2] !== "--")
+    //jezeli poprzednim ruchem byla strata, funkcja dublowalaby sie
+    [...word[3]].forEach(letter => addLetterInDeletion(letter));
+  [...document.querySelectorAll(".previous li")].forEach(node =>
+    previousRack.removeChild(node)
+  );
+  reset();
+
+  removePoints(word[4]);
+  let xy = putCoordinates(word[2]);
+  if (xy[2]) {
+    x = x + xy[1] * tileWidth;
+    y = y + xy[0] * tileWidth;
+  } else {
+    x = x + xy[0] * tileWidth;
+    y = y + xy[1] * tileWidth;
+  }
+  for (let i = 0; i < word[3].length; i++) {
+    let letter = word[3][i];
+    if (letter == ".") {
+      if (xy[2]) x += tileWidth;
+      else y += tileWidth;
+      continue;
+    }
+    ctx.clearRect(x, y, tileWidth, tileWidth);
+    if (xy[2]) x += tileWidth;
+    else y += tileWidth;
+  }
+  setRack();
+};
+
+const readGame = () => {
+  moves = [];
+  clearAll();
+  const lines = gameToString.split("\n");
+  const players = document.querySelectorAll(".players");
+  player1 = lines[0].split(" ")[1].replace(/_/g, " ");
+  player2 = lines[1].split(" ")[1].replace(/_/g, " ");
+  players[0].innerHTML = player1;
+  players[1].innerHTML = player2;
+  for (let i = 2; i < lines.length; i++) {
+    moves.push(lines[i]);
+  }
+  setRack();
+};
+readGame();
+document.getElementById("next").addEventListener("click", next);
+previousButton.addEventListener("click", clearMove);
+document.addEventListener("keydown", e => {
+  if (e.keyCode === 39) next();
+  if (e.keyCode === 37) clearMove();
+});
+document.body
+  .querySelector(".deletion-show")
+  .addEventListener("click", () => deletion.classList.toggle("active"));
+document
+  .querySelector(".close")
+  .addEventListener("click", () => deletion.classList.remove("active"));
+comment
+  .querySelector("button")
+  .addEventListener("click", () => (comment.style.display = "none"));
